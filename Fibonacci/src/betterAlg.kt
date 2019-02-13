f4fee8dcc7141efe44b9629f6bc016bf020e29e2
Владислав Гутник <gutnikvg@gmail.com>
2019-02-13 16:57:39 +0300

    Fibonacci better variant

--- Fibonacci/src/betterAlg.kt
+++ Fibonacci/src/betterAlg.kt
@@ -2,16 +2,18 @@ fun main() {
     println("Input a non-negative integer")
     val n: Int = readLine()?.toInt() ?: throw IllegalArgumentException("Got no input")
     checkInputNumber(n)
-    println(getFibonacciNumber(n))
+    print(getFibonacciNumber(n))
 }
 
 private fun getFibonacciNumber(n: Int): Int {
-    return if (n <= 1) n
-    else getFibonacciNumber(n - 1) + getFibonacciNumber(n - 2)
+    var fibonacciRow = emptyArray<Int>()
+    for (i in 0..n) {
+        if (i<=1) fibonacciRow += i
+        else fibonacciRow += fibonacciRow[i-1] + fibonacciRow[i-2]
+    }
+    return fibonacciRow[n]
 }
 
 private fun checkInputNumber(n: Int) {
     if (n<0) throw java.lang.IllegalArgumentException("Got a negative number")
 }
-
-
