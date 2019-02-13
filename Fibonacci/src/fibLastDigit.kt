650eacb70086c3a838fcf84e89a52639cefc8250
Владислав Гутник <gutnikvg@gmail.com>
2019-02-13 17:24:01 +0300

    Fibonacci number last digit

---
+++ Fibonacci/src/fibLastDigit.kt
@@ -0,0 +1,19 @@
+fun main() {
+    println("Input a non-negative integer")
+    val n: Int = readLine()?.toInt() ?: throw IllegalArgumentException("Got no input")
+    checkInputNumber(n)
+    print(getFibonacciNumber(n))
+}
+
+private fun getFibonacciNumber(n: Int): Int {
+    var fibonacciRow = mutableListOf<Int>()
+    for (i in 0..n) {
+        if (i<=1) fibonacciRow.add(i)
+        else fibonacciRow.add((fibonacciRow[i-1] + fibonacciRow[i-2]) % 10)
+    }
+    return fibonacciRow[n]
+}
+
+private fun checkInputNumber(n: Int) {
+    if (n<0) throw java.lang.IllegalArgumentException("Got a negative number")
+}
